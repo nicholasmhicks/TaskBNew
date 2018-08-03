c676ca213abdcccc2356aa604b15a0c8de71fce2
Dylan <dylan_1003@hotmail.com>
2018-08-03 12:59:59 +1000

    Added failing unit test

--- WebApplication/WebApplication.Tests/Controllers/ExamplesControllerTest.cs
+++ WebApplication/WebApplication.Tests/Controllers/ExamplesControllerTest.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication;
+using WebApplication.Controllers;
 
 namespace WebApplication.Tests.Controllers
 {
     [TestClass]
-    public class UnitTest1
+    public class ExamplesControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Get()
         {
+            // Arrange
+            ExamplesController controller = new ExamplesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("wrong", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
         }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
     }
 }
